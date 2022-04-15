627ca1beb793fb9f82f1889e414fd60bef63ee6b
Diego Masin <diegoifce@gmail.com>
2022-04-15 19:38:06 -0300

    Improving exception throwing on ExpressJs.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
 app.use(routes);
+
+// This middleware is necessary to be after the routes
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    // "next: nextFunction" is responsible for calling the next middleware or go ahead
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
