260e18dc4b03a37d1810628c01610190c1451d9c
Diego Masin <diegoifce@gmail.com>
2022-04-15 19:49:54 -0300

    Registration deliveryman.

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreCreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreCreateDeliveryman) {
+    // validate if deliveryman exists
+    const deliverymanExists = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (deliverymanExists) {
+      throw new Error("Deliveryman already exists");
+    }
+
+    // crypto password
+    const hashedPassword = await hash(password, 10);
+
+    // salve deliveryman
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+    return deliveryman;
+  }
+}
